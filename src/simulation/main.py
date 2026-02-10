4be6975e081de4e6761312c1080b7946c1e39cb9
pawel <paweljarosz002@gmail.com>
2026-02-10 22:08:38 +0100

    update plotter to compile gifs instead

--- src/simulation/main.py
+++ src/simulation/main.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from sim_config import SimConfig
 from sim_engine import SimEngine
-from sim_hooks import LivePlotter, log_progress
+from sim_hooks import GifPlotter, log_progress
 
 if __name__ == "__main__":
     config = SimConfig(DIM=2, N=100, dt=0.05, T_max=150.0)
@@ -9,11 +9,10 @@ if __name__ == "__main__":
 
     engine.register_hook(log_progress, every_n_steps=50)
 
-    plotter = LivePlotter()
+    plotter = GifPlotter("experiments/experiment1")
     engine.register_hook(plotter.update, every_n_steps=10)
 
     engine.run()
 
-    plt.ioff()
-    plt.show()
+    plotter.save_gif()
     
--- src/simulation/sim_hooks.py
+++ src/simulation/sim_hooks.py
@@ -1,19 +1,31 @@
 import matplotlib.pyplot as plt
 from sim_engine import SimState
-
+import os
+import shutil
+import imageio
 
 def log_progress(state: SimState):
     mass = state.total_mass
     print(f"[Step {state.step_iteration}] t={state.t:.2f} | Tumor Mass: {mass:.4f}")
 
 
-class LivePlotter:
-    def __init__(self):
+class GifPlotter:
+    def __init__(self, save_path: str):
+        self.save_path = save_path
+        self.frames_dir = os.path.join(self.save_path, 'frames')
+
+        # if directory exists, clear it
+        if os.path.exists(self.frames_dir):
+            shutil.rmtree(self.frames_dir)
+
+        os.makedirs(self.frames_dir)
+
         self.fig, self.ax = plt.subplots()
         self.im = None
-        plt.ion()
+        self.frame_paths = []
+        self.frame_count = 0
 
-    def update(self, state: SimState):
+    def update(self, state):
         if self.im is None:
             self.im = self.ax.imshow(
                 state.u, origin="lower", vmin=0, vmax=1, cmap="inferno"
@@ -26,3 +38,21 @@ class LivePlotter:
 
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
+
+        filename = os.path.join(self.frames_dir, f"frame_{self.frame_count:05d}.png")
+        
+        self.fig.savefig(filename)
+        self.frame_paths.append(filename)
+        self.frame_count += 1
+
+    def save_gif(self, fps: int = 10):
+        gif_path = os.path.join(self.save_path, "simulation.gif")
+        print(f"Compiling {len(self.frame_paths)} frames into {gif_path}...")
+
+        with imageio.get_writer(gif_path, mode='I', fps=fps) as writer:
+            for filename in self.frame_paths:
+                image = imageio.imread(filename)
+                writer.append_data(image)
+        
+        print("GIF saved successfully.")
+
